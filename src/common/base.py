77055b2a6e6b7076657d8bd4b4a9bc6503cf0ff1
Jim Broadbent <jim@encord.com>
2024-02-12 17:50:30 +0000

    Moved embedding logic away from CLIP class. Introduced ability to build embedding into Embeddings class. Non circular

--- src/common/base.py
+++ src/common/base.py
@@ -1,9 +1,15 @@
+from pathlib import Path
+
+import numpy as np
+import torch
+from constants import NPZ_KEYS
+from dataset import HFDataset
+from models import CLIPModel
 from pydantic import BaseModel, model_validator
+from torch.utils.data import DataLoader
+from tqdm.auto import tqdm
 
 from common.numpy_types import ClassArray, EmbeddingArray
-from constants import NPZ_KEYS
-from pathlib import Path
-import numpy as np
 
 
 class Embeddings(BaseModel):
@@ -28,22 +34,32 @@ class Embeddings(BaseModel):
     @staticmethod
     def from_file(path: Path) -> "Embeddings":
         if not path.suffix == ".npz":
-            raise ValueError(f"Embedding files should be `.npz` files not {path.suffix}")
+            raise ValueError(
+                f"Embedding files should be `.npz` files not {path.suffix}"
+            )
 
         loaded = np.load(path)
         if NPZ_KEYS.IMAGE_EMBEDDINGS not in loaded and NPZ_KEYS.LABELS not in loaded:
-            raise ValueError(f"Require both {NPZ_KEYS.IMAGE_EMBEDDINGS}, {NPZ_KEYS.LABELS} should be present in {path}")
+            raise ValueError(
+                f"Require both {NPZ_KEYS.IMAGE_EMBEDDINGS}, {NPZ_KEYS.LABELS} should be present in {path}"
+            )
 
         image_embeddings: EmbeddingArray = loaded[NPZ_KEYS.IMAGE_EMBEDDINGS]
         labels: ClassArray = loaded[NPZ_KEYS.LABELS]
         label_embeddings: EmbeddingArray | None = (
-            loaded[NPZ_KEYS.CLASS_EMBEDDINGS] if NPZ_KEYS.CLASS_EMBEDDINGS in loaded else None
+            loaded[NPZ_KEYS.CLASS_EMBEDDINGS]
+            if NPZ_KEYS.CLASS_EMBEDDINGS in loaded
+            else None
         )
-        return Embeddings(images=image_embeddings, labels=labels, classes=label_embeddings)
+        return Embeddings(
+            images=image_embeddings, labels=labels, classes=label_embeddings
+        )
 
     def to_file(self, path: Path) -> Path:
         if not path.suffix == ".npz":
-            raise ValueError(f"Embedding files should be `.npz` files not {path.suffix}")
+            raise ValueError(
+                f"Embedding files should be `.npz` files not {path.suffix}"
+            )
         to_store: dict[str, EmbeddingArray] = {
             NPZ_KEYS.IMAGE_EMBEDDINGS: self.images,
             NPZ_KEYS.LABELS: self.labels,
@@ -57,6 +73,51 @@ class Embeddings(BaseModel):
             **to_store,
         )
         return path
+
+    @staticmethod
+    def from_embedding_definition(model_name: str, dataset_name: str) -> "Embeddings":
+        try:
+            model = CLIPModel.load_model(model_name)
+        except Exception:
+            raise Exception
+        try:
+            dataset = HFDataset(dataset_name)
+        except Exception:
+            raise Exception
+        embeddings = Embeddings.build_embedding(model, dataset)
+        return embeddings
+
+    @staticmethod
+    def build_embedding(
+        self, model: CLIPModel, dataset: HFDataset, batch_size: int = 50
+    ) -> "Embeddings":
+        def _collate_fn(examples) -> dict[str, torch.tensor]:
+            images = []
+            labels = []
+            for example in examples:
+                images.append(example["image"])
+                labels.append(examples["label"])
+
+            pixel_values = torch.stack(images)
+            labels = torch.tensor(labels)
+            return {"pixel_values": pixel_values, "labels": labels}
+
+        transformed_dataset = dataset.dataset.set_transform(self.process_fn)
+        dataloader = DataLoader(
+            transformed_dataset, collate_fn=_collate_fn, batch_size=batch_size
+        )
+        tmp_embeddings = []
+        tmp_labels = []
+        with torch.inference_mode():
+            for batch in tqdm(dataloader, desc=f"Embedding dataset with {self.title}"):
+                tmp_labels.append(batch["label"])
+                features = model.get_image_features(pixel_values=batch["pixel_values"])
+                emb = (features / features.norm(p=2, dim=-1, keepdim=True)).squeeze()
+                tmp_embeddings.append(emb)
+        embeddings: EmbeddingArray = np.concatenate(tmp_embeddings, 0)
+        labels: ClassArray = np.array(tmp_labels)
+        embeddings = Embeddings(images=embeddings, labels=labels)
+        return embeddings
 
     class Config:
         arbitrary_types_allowed = True
--- src/common/data_models.py
+++ src/common/data_models.py
@@ -2,15 +2,13 @@ import logging
 from pathlib import Path
 
 import numpy as np
+from constants import PROJECT_PATHS
 from pydantic import BaseModel
 from pydantic.functional_validators import AfterValidator
 from typing_extensions import Annotated
 
 from common.base import Embeddings
 from common.string_utils import safe_str
-from constants import PROJECT_PATHS
-from dataset import HFDataset
-from models import CLIPModel
 
 SafeName = Annotated[str, AfterValidator(safe_str)]
 logger = logging.getLogger("multiclips")
@@ -28,7 +26,9 @@ class EmbeddingDefinition(BaseModel):
         return PROJECT_PATHS.EMBEDDINGS / self._get_embedding_path(".npz")
 
     def get_reduction_path(self, reduction_name: str):
-        return PROJECT_PATHS.REDUCTIONS / self._get_embedding_path(f".{reduction_name}.2d.npy")
+        return PROJECT_PATHS.REDUCTIONS / self._get_embedding_path(
+            f".{reduction_name}.2d.npy"
+        )
 
     def load_embeddings(self) -> Embeddings | None:
         """
@@ -59,33 +59,8 @@ class EmbeddingDefinition(BaseModel):
         embeddings.to_file(self.embedding_path)
         return True
 
-    def build_embedding(self, overwrite=False) -> bool:
-        if self.embedding_path.is_file() and not overwrite:
-            return False
-        try:
-            self.model = self._load_model()
-        except Exception:
-            raise Exception
-        try:
-            self.dataset = self._load_dataset()
-        except Exception:
-            raise Exception
-
-        embeddings: Embeddings = self.model.embed(self.dataset)
-        return self.save_embeddings(embeddings)
-
-    # Possible that the below methods should return objects rather than modifying state
-    def _load_model(self) -> CLIPModel:
-        try:
-            return CLIPModel.load_model(model_name=self.model)
-        except Exception as e:
-            raise e
-
-    def _load_dataset(self) -> HFDataset:
-        try:
-            return HFDataset(dataset_name=self.dataset)
-        except Exception as e:
-            raise e
+    def build_embedding(self) -> Embeddings:
+        return EmbeddingDefinition.from_embedding_definition(self.model, self.dataset)
 
 
 if __name__ == "__main__":
