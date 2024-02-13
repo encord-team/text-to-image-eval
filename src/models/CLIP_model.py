3351089c9a4e309f9308acd5a7fe9cb1c36acecd
Jim-Encord <156204297+Jim-Encord@users.noreply.github.com>
2024-02-13 10:41:38 +0000

    Refactor embedding creation logic. (#4)
    Model download/initialisation it's working now.
    
    Known issues: Datasets is not working.

--- src/models/CLIP_model.py
+++ src/models/CLIP_model.py
@@ -1,12 +1,7 @@
 import numpy as np
 import torch
-from torch.utils.data import DataLoader
-from tqdm.auto import tqdm
 from transformers import CLIPModel as HF_ClipModel
 from transformers import CLIPProcessor
-
-from dataset import HFDataset
-from common.base import ClassArray, EmbeddingArray, Embeddings
 
 options = {
     "clip": "openai/clip-vit-large-patch14-336",
@@ -23,35 +18,24 @@ model_name = options[short_name]
 
 
 class CLIPModel:
-    def __init__(self, model_name: str, device: torch.Device = torch.device("cuda")) -> None:
+    def __init__(self, model_name: str, device: str = torch.device("cpu")) -> None:
         self.model = HF_ClipModel.from_pretrained(model_name).to(device)
         self.title = model_name
+        self.processor = CLIPProcessor.from_pretrained(model_name)
+        self.process_fn = self.define_process_fn()
 
-    def embed(self, dataset: HFDataset, batch_size: int = 50) -> Embeddings:
-        def _collate_fn(examples) -> dict[str, torch.tensor]:
-            images = []
-            labels = []
-            for example in examples:
-                images.append(example["image"])
-                labels.append(examples["label"])
+    def define_process_fn(self):
+        def process_fn(batch):
+            images = [i.convert("RGB") for i in batch["image"]]
+            batch["image"] = [
+                self.processor(images=[i], return_tensors="pt")
+                .to("cuda")
+                .pixel_values.squeeze()
+                for i in images
+            ]
+            return batch
 
-            pixel_values = torch.stack(images)
-            labels = torch.tensor(labels)
-            return {"pixel_values": pixel_values, "labels": labels}
-
-        dataloader = DataLoader(dataset, collate_fn=_collate_fn, batch_size=batch_size)
-        tmp_embeddings = []
-        tmp_labels = []
-        with torch.inference_mode():
-            for batch in tqdm(dataloader, desc=f"Embedding dataset with {self.title}"):
-                tmp_labels.append(batch["label"])
-                features = self.model.get_image_features(pixel_values=batch["pixel_values"])
-                emb = (features / features.norm(p=2, dim=-1, keepdim=True)).squeeze()
-                tmp_embeddings.append(emb)
-        embeddings: EmbeddingArray = np.concatenate(tmp_embeddings, 0)
-        labels: ClassArray = np.array(tmp_labels)
-        embeddings = Embeddings(images=embeddings, labels=labels)
-        return embeddings
+        return process_fn
 
     @classmethod
     def load_model(cls, model_name: str):
