4888e5c46614bc76eb103a27dac15344279db55a
Eloy Pérez Torres <99720527+eloy-encord@users.noreply.github.com>
2024-02-14 11:54:07 +0000

    chore: clean dataset and models code (#11)

--- src/dataset/dataset.py
+++ src/dataset/dataset.py
@@ -1,4 +1,4 @@
-from datasets import Dataset, DatasetDict, load_dataset
+from datasets import Dataset, Split, load_dataset
 
 DATASETS = {"LungCancer4Types": "Kabil007/LungCancer4Types", "plants": "sampath017/plants"}
 
@@ -6,16 +6,13 @@ DATASETS = {"LungCancer4Types": "Kabil007/LungCancer4Types", "plants": "sampath0
 class HFDataset:
     def __init__(self, dataset_name: str) -> None:
         if dataset_name not in DATASETS:
-            raise ValueError("Unsupported dataset")
+            raise ValueError(f"Unrecognized dataset: {dataset_name}")
         dataset_name = DATASETS[dataset_name]
         self.dataset = self._load_dataset(dataset_name)
 
     def _load_dataset(self, dataset_name) -> Dataset:
         try:
-            dataset = load_dataset(dataset_name)
+            dataset = load_dataset(dataset_name, split=Split.TRAIN)
         except Exception as e:
-            self.dataset = None
-            raise ValueError(f"Failed to load dataset from HF {dataset_name}") from e
-        if isinstance(dataset, DatasetDict):
-            dataset = dataset["train"]
+            raise ValueError(f"Failed to load dataset from Hugging Face: {dataset_name}") from e
         return dataset
