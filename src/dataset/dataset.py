cb8880c2f6c5989baf9e78fb7c64fcdd728a8fdc
Eloy Pérez Torres <eloy@encord.com>
2024-02-21 15:02:01 +0100

    misc: add missing kwargs in abstract method
    Also, enforces transform param as keyword only.

--- src/dataset/dataset.py
+++ src/dataset/dataset.py
@@ -5,7 +5,7 @@ from torch.utils.data import Dataset as TorchDataset
 
 
 class Dataset(TorchDataset, ABC):
-    def __init__(self, title: str, title_in_source: str | None = None, transform=None, **kwargs):
+    def __init__(self, title: str, title_in_source: str | None = None, *, transform=None, **kwargs):
         self.transform = transform
         self.__title = title
         self.__title_in_source = title if title_in_source is None else title_in_source
@@ -30,13 +30,13 @@ class Dataset(TorchDataset, ABC):
         self.transform = transform
 
     @abstractmethod
-    def _setup(self):
+    def _setup(self, **kwargs):
         pass
 
 
 class HFDataset(Dataset):
-    def __init__(self, title: str, title_in_source: str | None = None, transform=None, **kwargs):
-        super().__init__(title, title_in_source, transform)
+    def __init__(self, title: str, title_in_source: str | None = None, *, transform=None, **kwargs):
+        super().__init__(title, title_in_source, transform=transform)
         self._setup(**kwargs)
 
     def __getitem__(self, idx):
