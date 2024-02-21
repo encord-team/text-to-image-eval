32e0dcbf3593365fa6c98ea0ac874144e7594bc9
Eloy Pérez Torres <99720527+eloy-encord@users.noreply.github.com>
2024-02-21 14:27:58 +0000

    feat: allow multiple dataset sources (#22)
    Adds a dataset provider, following the factory method pattern, and a
    dataset class abstraction to enable dataset extraction from several
    sources.
    
    Atm, only one interface is available and it fetches datasets from
    Hugging Face.
    Support for Encord datasets will arrive shortly in the next iteration.

---
+++ src/dataset/provider.py
@@ -0,0 +1,30 @@
+from src.dataset.dataset import Dataset, HFDataset
+
+
+class DatasetProvider:
+    def __init__(self):
+        self._datasets = {}
+
+    def register_dataset(self, title: str, source: type[Dataset], **kwargs):
+        self._datasets[title] = (source, kwargs)
+
+    def get_dataset(self, title: str) -> Dataset:
+        if title not in self._datasets:
+            raise ValueError(f"Unrecognized dataset: {title}")
+        source, kwargs = self._datasets[title]
+        return source(title, **kwargs)
+
+
+dataset_provider = DatasetProvider()
+dataset_provider.register_dataset("LungCancer4Types", HFDataset, title_in_source="Kabil007/LungCancer4Types")
+dataset_provider.register_dataset("plants", HFDataset, title_in_source="sampath017/plants")
+dataset_provider.register_dataset("NIH-Chest-X-ray", HFDataset, title_in_source="alkzar90/NIH-Chest-X-ray-dataset")
+dataset_provider.register_dataset("Alzheimer-MRI", HFDataset, title_in_source="Falah/Alzheimer_MRI")
+dataset_provider.register_dataset("Skin-cancer", HFDataset, title_in_source="marmal88/skin_cancer")
+dataset_provider.register_dataset(
+    "chest-xray-classification", HFDataset, title_in_source="tpakov/chest-xray-classification"
+)
+dataset_provider.register_dataset(
+    "sports-classification", HFDataset, title_in_source="HES-XPLAIN/SportsImageClassification"
+)
+dataset_provider.register_dataset("geo-landmarks", HFDataset, title_in_source="Qdrant/google-landmark-geo")
