b07b9290de6163d8c23367567c87449811303b37
Frederik Hvilshøj <frederik@cord.tech>
2024-02-22 15:46:34 +0100

    add typer cli to make things less convoluted

--- clip_eval/cli.py
+++ clip_eval/cli.py
@@ -1,25 +1,19 @@
-import argparse
-
 import matplotlib.pyplot as plt
+from typer import Argument, Option, Typer
+from typing_extensions import Annotated
 
 from clip_eval.common.data_models import EmbeddingDefinition
-from clip_eval.dataset.provider import dataset_provider
-from clip_eval.evaluation import (
-    LinearProbeClassifier,
-    WeightedKNNClassifier,
-    ZeroShotClassifier,
-)
-from clip_eval.evaluation.evaluator import export_evaluation_to_csv, run_evaluation
-from clip_eval.models import model_provider
-from clip_eval.plotting.animation import build_animation, save_animation_to_file
 from clip_eval.utils import read_all_cached_embeddings
 
-
-def build_command(args):
-    build_embedding(args.model_dataset)
+cli = Typer(name="clip-eval", no_args_is_help=True, rich_markup_mode="markdown")
 
 
-def build_embedding(model_dataset: str):
+@cli.command("build", help="Build embeddings")
+def build_command(
+    model_dataset: Annotated[
+        str, Argument(help="model, dataset pair delimited by model/dataset")
+    ]
+):
     if model_dataset.count("/") != 1:
         raise ValueError("model dataset must contain only 1 /")
     model, dataset = model_dataset.split("/")
@@ -30,18 +24,34 @@ def build_embedding(model_dataset: str):
     print("Saved embedding to file successfully at", embd_defn.embedding_path)
 
 
-def evaluate_command(args):
-    evaluate_embeddings(args.model_datasets, args.all, args.save)
+@cli.command("evaluate", help="Evaluate embedding performance")
+def evaluate_embeddings(
+    model_datasets: Annotated[
+        list[str], Option(help="Specify specific combinations of models and datasets")
+    ] = [],
+    is_all: Annotated[bool, Option(help="Evaluate all models.")] = False,
+    save: Annotated[bool, Option(help="")] = False,
+):
+    from clip_eval.evaluation import (
+        LinearProbeClassifier,
+        WeightedKNNClassifier,
+        ZeroShotClassifier,
+    )
+    from clip_eval.evaluation.evaluator import export_evaluation_to_csv, run_evaluation
 
-
-def evaluate_embeddings(model_datasets: list[str] | None = None, is_all: bool = False, save: bool = False):
     if is_all:
-        defns: list[EmbeddingDefinition] = [d for k, v in read_all_cached_embeddings().items() for d in v]
+        defns: list[EmbeddingDefinition] = [
+            d for k, v in read_all_cached_embeddings().items() for d in v
+        ]
     else:
         # Error could be localised better
         if not all([model_dataset.count("/") == 1 for model_dataset in model_datasets]):
-            raise ValueError("All model,dataset pairs must be presented as MODEL/DATASET")
-        model_dataset_pairs = [model_dataset.split("/") for model_dataset in model_datasets]
+            raise ValueError(
+                "All model,dataset pairs must be presented as MODEL/DATASET"
+            )
+        model_dataset_pairs = [
+            model_dataset.split("/") for model_dataset in model_datasets
+        ]
         defns = [
             EmbeddingDefinition(model=model_dataset[0], dataset=model_dataset[1])
             for model_dataset in model_dataset_pairs
@@ -53,82 +63,50 @@ def evaluate_embeddings(model_datasets: list[str] | None = None, is_all: bool = 
     print(performances)
 
 
-def animate_command(args):
-    animate_embeddings(args.model_datasets)
+@cli.command(
+    "animate", help="2 (shortened) model dataset pairs written as model/dataset"
+)
+def animate_embeddings(
+    from_model_dataset: Annotated[str, Argument(help="model/dataset pair")],
+    to_model_dataset: Annotated[str, Argument(help="model/dataset pair")],
+):
+    from clip_eval.plotting.animation import build_animation, save_animation_to_file
 
-
-def animate_embeddings(model_datasets: list[str]):
     # Error could be localised better
+    model_datasets = [from_model_dataset, to_model_dataset]
     assert len(model_datasets) == 2
     if not all([model_dataset.count("/") == 1 for model_dataset in model_datasets]):
         raise ValueError("All model,dataset pairs must be presented as MODEL/DATASET")
     model_dataset_pairs = [model_dataset.split("/") for model_dataset in model_datasets]
     assert len(model_dataset_pairs) == 2 and len(model_dataset_pairs[0]) == 2
     defns = [
-        EmbeddingDefinition(model=model_dataset[0], dataset=model_dataset[1]) for model_dataset in model_dataset_pairs
+        EmbeddingDefinition(model=model_dataset[0], dataset=model_dataset[1])
+        for model_dataset in model_dataset_pairs
     ]
     anim = build_animation(*defns)
     save_animation_to_file(anim, *defns)
     plt.show()
 
 
-def list_command(args):
-    list_models_datasets(args.all)
+@cli.command("list", help="List models and datasets")
+def list_models_datasets(
+    all: Annotated[bool, Option(help="List all available models and dataset")] = False
+):
+    from clip_eval.dataset.provider import dataset_provider
+    from clip_eval.models import model_provider
 
-
-def list_models_datasets(all: bool = False):
     if all:
         datasets = dataset_provider.list_dataset_names()
         models = model_provider.list_model_names()
         print(f"Available datasets are: {', '.join(datasets)}")
         print(f"Available models are: {', '.join(models)}")
         return
-    else:
-        defns: list[EmbeddingDefinition] = [d for k, v in read_all_cached_embeddings().items() for d in v]
-        print(f"Available model_dataset pairs: {', '.join([str(defn) for defn in defns])}")
 
-
-def main():
-    parser = argparse.ArgumentParser(description="Multiclip builder and Benchmarker")
-    subparsers = parser.add_subparsers()
-
-    parser_build = subparsers.add_parser("build", help="build embeddings")
-
-    parser_build.add_argument(
-        "model_dataset",
-        type=str,
-        help="model, dataset pair delimited by model/dataset",
-        default=None,
-    )
-    parser_build.set_defaults(func=build_command)
-
-    parser_evaluate = subparsers.add_parser("evaluate", help="evaluate embeddings")
-    parser_evaluate.add_argument("--all", action="store_true", help="Evaluate all models")
-    parser_evaluate.add_argument(
-        "model_datasets",
-        metavar="model/dataset",
-        nargs="+",
-        help="(shortened) model dataset pairs written as model/dataset",
-    )
-    parser_evaluate.add_argument("--save", action="store_true", help="Save report to CSV")
-    parser_evaluate.set_defaults(func=evaluate_command)
-
-    parser_animation = subparsers.add_parser("animate", help="animate between two embeddings")
-    parser_animation.add_argument(
-        "model_datasets",
-        metavar="model/dataset",
-        nargs=2,
-        help="2 (shortened) model dataset pairs written as model/dataset",
-    )
-
-    parser_animation.set_defaults(func=animate_command)
-
-    parser_list = subparsers.add_parser("list", help="List available dataset, model pairs")
-    parser_list.add_argument("--all", action="store_true", help="List all available models and dataset")
-    parser_list.set_defaults(func=list_command)
-    args = parser.parse_args()
-    args.func(args)
+    defns: list[EmbeddingDefinition] = [
+        d for k, v in read_all_cached_embeddings().items() for d in v
+    ]
+    print(f"Available model_dataset pairs: {', '.join([str(defn) for defn in defns])}")
 
 
 if __name__ == "__main__":
-    main()
+    cli()
