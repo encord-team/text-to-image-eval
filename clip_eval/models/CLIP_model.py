c52f81dd53d561d47418701ec86064863edfc526
Jim-Encord <156204297+Jim-Encord@users.noreply.github.com>
2024-02-26 12:28:44 +0000

    feat: Siglip working (#33)
    Included a small and large model. Tested on plants dataset locally.
    Code is verbatim duplicated from Closed_CLIPmodel, so could be
    refactored and given that transformers >= 4.37, Siglip is supported in
    transformers.
    Had to add sentencepiece + protobuf as a dependency. Google pushing
    google products seemingly.

--- clip_eval/models/CLIP_model.py
+++ clip_eval/models/CLIP_model.py
@@ -8,6 +8,8 @@ from torch.utils.data import DataLoader
 from tqdm import tqdm
 from transformers import CLIPModel as HF_ClipModel
 from transformers import CLIPProcessor as HF_ClipProcessor
+from transformers import SiglipModel as HF_SiglipModel
+from transformers import SiglipProcessor as HF_SiglipProcessor
 
 from clip_eval.common.numpy_types import ClassArray, EmbeddingArray
 
@@ -64,7 +66,7 @@ class closed_CLIPModel(CLIPModel):
         super().__init__(title, title_in_source, device)
         self._setup()
 
-    def define_process_fn(self):
+    def define_process_fn(self) -> Callable[[dict[str, Any]], dict[str, list[Any]]]:
         def process_fn(batch) -> dict[str, list[Any]]:
             images = [i.convert("RGB") for i in batch["image"]]
             batch["image"] = [
@@ -117,3 +119,41 @@ class open_CLIPModel(CLIPModel):
 
     def get_transform(self) -> Callable[[Any], Any]:
         raise NotImplementedError("open Clip not implemented")
+
+
+class SiglipModel(CLIPModel):
+    def __init__(self, title: str, title_in_source: str | None = None, device: str | None = None, **kwargs) -> None:
+        super().__init__(title, title_in_source, device, **kwargs)
+        self._setup()
+
+    def _setup(self, **kwargs):
+        self.model = HF_SiglipModel.from_pretrained(self.title_in_source)
+        self.processor = HF_SiglipProcessor.from_pretrained(self.title_in_source)
+        self.process_fn = self.define_process_fn()
+
+    def define_process_fn(self) -> Callable[[dict[str, Any]], dict[str, list[Any]]]:
+        def process_fn(batch) -> dict[str, list[Any]]:
+            images = [i.convert("RGB") for i in batch["image"]]
+            batch["image"] = [
+                self.processor(images=[i], return_tensors="pt").to(self.device).pixel_values.squeeze() for i in images
+            ]
+            return batch
+
+        return process_fn
+
+    def build_embedding(self, dataloader: DataLoader) -> tuple[EmbeddingArray, ClassArray]:
+        tmp_embeddings = []
+        tmp_labels = []
+        with torch.inference_mode():
+            for batch in tqdm(dataloader, desc=f"Embedding dataset with {self.title}"):
+                tmp_labels.append(batch["labels"])
+                features = self.model.get_image_features(pixel_values=batch["pixel_values"])
+                emb = (features / features.norm(p=2, dim=-1, keepdim=True)).squeeze()
+                tmp_embeddings.append(emb.to("cpu"))
+        image_embeddings: EmbeddingArray = np.concatenate(tmp_embeddings, 0)
+        class_array = torch.concatenate(tmp_labels)
+        labels = class_array.numpy()
+        return image_embeddings, labels
+
+    def get_transform(self) -> Callable[[Any], Any]:
+        return self.process_fn
