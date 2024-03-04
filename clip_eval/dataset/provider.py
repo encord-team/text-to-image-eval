a2d73154ea8f50c41eae74ed85de3dd692fe0b4d
Eloy Pérez Torres <eloy@encord.com>
2024-03-04 11:54:12 +0100

    feat: add global cache configuration for datasets

--- clip_eval/dataset/provider.py
+++ clip_eval/dataset/provider.py
@@ -1,10 +1,24 @@
 from .dataset import Dataset, HFDataset
 from .encord_dataset import EncordDataset
+from typing import Any
+from copy import deepcopy
+from clip_eval.constants import _CACHE_PATH
 
 
 class DatasetProvider:
     def __init__(self):
         self._datasets = {}
+        self._global_settings: dict[str, Any] = dict()
+
+    @property
+    def global_settings(self) -> dict:
+        return deepcopy(self._global_settings)
+
+    def add_global_setting(self, name: str, value: Any) -> None:
+        self._global_settings[name] = value
+
+    def remove_global_setting(self, name: str):
+        self._global_settings.pop(name, None)
 
     def register_dataset(self, title: str, source: type[Dataset], **kwargs):
         self._datasets[title] = (source, kwargs)
@@ -13,13 +27,18 @@ class DatasetProvider:
         if title not in self._datasets:
             raise ValueError(f"Unrecognized dataset: {title}")
         source, kwargs = self._datasets[title]
-        return source(title, **kwargs)
+        # Apply global settings. Values of local settings take priority when local and global settings share keys.
+        kwargs_with_global_settings = self.global_settings | kwargs
+        return source(title, **kwargs_with_global_settings)
 
     def list_dataset_names(self) -> list[str]:
         return list(self._datasets.keys())
 
 
 dataset_provider = DatasetProvider()
+# Add global settings
+dataset_provider.add_global_setting("cache_dir", _CACHE_PATH)
+
 dataset_provider.register_dataset("LungCancer4Types", HFDataset, title_in_source="Kabil007/LungCancer4Types")
 dataset_provider.register_dataset("plants", HFDataset, title_in_source="sampath017/plants")
 dataset_provider.register_dataset(
