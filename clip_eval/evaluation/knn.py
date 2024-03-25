e74933b7d3d0c84c832a9fcbde1161862d321de1
Eloy Pérez Torres <eloy@encord.com>
2024-03-25 12:09:13 +0100

    feat: update knn classification model

--- clip_eval/evaluation/knn.py
+++ clip_eval/evaluation/knn.py
@@ -5,7 +5,7 @@ import numpy as np
 from autofaiss import build_index
 
 from clip_eval.common.data_models import Embeddings
-from clip_eval.common.numpy_types import ClassArray, EmbeddingArray, ProbabilityArray
+from clip_eval.common.numpy_types import ClassArray, ProbabilityArray
 from clip_eval.evaluation.base import ClassificationModel
 
 logger = logging.getLogger("multiclips")
@@ -14,9 +14,8 @@ logger = logging.getLogger("multiclips")
 class WeightedKNNClassifier(ClassificationModel):
     def __init__(
         self,
-        embeddings: EmbeddingArray,
-        labels: ClassArray,
-        class_embeddings: EmbeddingArray | None = None,
+        train_embeddings: Embeddings,
+        validation_embeddings: Embeddings,
         num_classes: int | None = None,
         k: int = 11,
     ) -> None:
@@ -35,15 +34,10 @@ class WeightedKNNClassifier(ClassificationModel):
         Raises:
             ValueError: If the faiss index fails to build.
         """
-        super().__init__("wKNN")
-        self._labels = labels
+        super().__init__("wKNN", train_embeddings, validation_embeddings, num_classes)
         self.k = k
 
-        self.num_classes = num_classes or labels.max() + 1
-
-        embeddings = self.normalize(embeddings)
-        index, self.index_infos = build_index(embeddings, save_on_disk=False, verbose=logging.ERROR)
-
+        index, self.index_infos = build_index(train_embeddings.images, save_on_disk=False, verbose=logging.ERROR)
         if index is None:
             raise ValueError("Failed to build an index for knn search")
         self._index = index
@@ -54,20 +48,13 @@ class WeightedKNNClassifier(ClassificationModel):
     def get_default_params() -> dict[str, Any]:
         return {"k": 11}
 
-    @property
-    def dim(self) -> int:
-        return self._index.d
-
-    def predict(self, embeddings: Embeddings) -> tuple[ProbabilityArray, ClassArray]:
-        super()._check_dims(embeddings)
-        n, *_ = embeddings.images.shape
-        img_embeddings = self.normalize(embeddings.images)
-
-        dists, nearest_indices = self._index.search(img_embeddings, self.k)  # type: ignore
-        nearest_classes = np.take(self._labels, nearest_indices)
+    def predict(self) -> tuple[ProbabilityArray, ClassArray]:
+        dists, nearest_indices = self._index.search(self._val_embeddings.images, self.k)  # type: ignore
+        nearest_classes = np.take(self._train_embeddings.labels, nearest_indices)
 
         # NOTE: if self.k and self.num_classes are both large, this might become a big one.
         # We can shape of a factor self.k if we count differently here.
+        n = len(self._val_embeddings.images)
         weighted_count = np.zeros((n, self.num_classes, self.k), dtype=np.float32)
         weighted_count[
             np.tile(np.arange(n), (self.k,)).reshape(-1),  # [0, 0, .., 0_k, 1, 1, .., 1_k, ..]
@@ -79,15 +66,19 @@ class WeightedKNNClassifier(ClassificationModel):
 
 
 if __name__ == "__main__":
-    knn = WeightedKNNClassifier(
-        np.random.randn(100, 10).astype(np.float32),
-        np.random.randint(0, 10, size=(100,)),
-        num_classes=10,
+    train_embeddings = Embeddings(
+        images=np.random.randn(100, 10).astype(np.float32),
+        labels=np.random.randint(0, 10, size=(100,)),
     )
-    embeddings = Embeddings(
+    val_embeddings = Embeddings(
         images=np.random.randn(2, 10).astype(np.float32),
         labels=np.random.randint(0, 10, size=(2,)),
     )
-    probs, cls = knn.predict(embeddings)
+    knn = WeightedKNNClassifier(
+        train_embeddings,
+        val_embeddings,
+        num_classes=10,
+    )
+    probs, pred_classes = knn.predict()
     print(probs)
-    print(cls)
+    print(pred_classes)
