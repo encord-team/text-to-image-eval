c8927723206c6ffc7e291823e2b036f6dcf4172b
Eloy Pérez Torres <eloy@encord.com>
2024-03-26 13:38:34 +0100

    fix: resolve division by zero in wKNN

--- clip_eval/evaluation/knn.py
+++ clip_eval/evaluation/knn.py
@@ -58,6 +58,9 @@ class WeightedKNNClassifier(ClassificationModel):
         dists, nearest_indices = self._index.search(self._val_embeddings.images, self.k)  # type: ignore
         nearest_classes = np.take(self._train_embeddings.labels, nearest_indices)
 
+        # Calculate class votes from the distances (avoiding division by zero)
+        max_value = np.finfo(np.float32).max
+        scores = np.where(dists.reshape(-1) == 0, max_value, 1 / np.square(dists.reshape(-1)))
         # NOTE: if self.k and self.num_classes are both large, this might become a big one.
         # We can shape of a factor self.k if we count differently here.
         n = len(self._val_embeddings.images)
@@ -66,7 +69,7 @@ class WeightedKNNClassifier(ClassificationModel):
             np.tile(np.arange(n), self.k),  # [0, 0, .., 0_k, 1, 1, .., 1_k, ..]
             nearest_classes.reshape(-1),  # [class numbers]
             np.tile(np.arange(self.k), n),  # [0, 1, .., k-1, 0, 1, .., k-1, ..]
-        ] = 1 / np.square(dists.reshape(-1))
+        ] = scores
         probabilities = self.softmax(weighted_count.sum(-1))
         return probabilities, np.argmax(probabilities, axis=1)
 
