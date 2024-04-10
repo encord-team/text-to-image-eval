4b5c7f63eb9d7b39f2c25c5312e9a2994d79a8e5
Eloy Pérez Torres <eloy@encord.com>
2024-04-10 13:03:29 +0200

    feat: add dataset registration from json definition v1

--- clip_eval/dataset/provider.py
+++ clip_eval/dataset/provider.py
@@ -1,9 +1,12 @@
+import json
 from copy import deepcopy
+from pathlib import Path
 from typing import Any
 
 from natsort import natsorted, ns
 
 from clip_eval.constants import CACHE_PATH
+from clip_eval.utils import load_class_from_path
 
 from .base import Dataset, Split
 from .encord import EncordDataset
@@ -14,6 +17,7 @@ class DatasetProvider:
     def __init__(self):
         self._datasets = {}
         self._global_settings: dict[str, Any] = dict()
+        self.__known_dataset_types: dict[tuple[str, str], Any] = dict()
 
     @property
     def global_settings(self) -> dict:
@@ -22,10 +26,10 @@ class DatasetProvider:
     def add_global_setting(self, name: str, value: Any) -> None:
         self._global_settings[name] = value
 
-    def remove_global_setting(self, name: str):
+    def remove_global_setting(self, name: str) -> None:
         self._global_settings.pop(name, None)
 
-    def register_dataset(self, source: type[Dataset], title: str, split: Split | None = None, **kwargs):
+    def register_dataset(self, source: type[Dataset], title: str, split: Split | None = None, **kwargs) -> None:
         if split is None:
             # One dataset with all the split definitions
             self._datasets[title] = (source, kwargs)
@@ -33,6 +37,32 @@ class DatasetProvider:
             # One dataset is defined per split
             kwargs.update(split=split)
             self._datasets[(title, split)] = (source, kwargs)
+
+    def register_dataset_from_json_definition(self, json_definition: Path) -> None:
+        dataset_params: dict = json.loads(json_definition.read_text(encoding="utf-8"))
+        module_path: str | None = dataset_params.pop("module_path", None)
+        dataset_type_name: str | None = dataset_params.pop("dataset_type", None)
+        if module_path is None or dataset_type_name is None:
+            raise ValueError(
+                f"Missing required fields `module_path` or `dataset_type` in "
+                f"the JSON definition file: {json_definition.as_posix()}"
+            )
+
+        # Fetch the class of the dataset type stated in the definition
+        dataset_type = self.__known_dataset_types.get((module_path, dataset_type_name))
+        if dataset_type is None:
+            dataset_type = load_class_from_path(module_path, dataset_type_name)
+            if not issubclass(dataset_type, Dataset):
+                raise ValueError(
+                    f"Dataset type specified in the JSON definition file `{json_definition.as_posix()}` "
+                    f"does not inherit from the base class `Dataset`"
+                )
+            self.__known_dataset_types[(module_path, dataset_type_name)] = dataset_type
+        self.register_dataset(dataset_type, **dataset_params)
+
+    def register_datasets_from_sources_dir(self, source_dir: Path) -> None:
+        for f in source_dir.glob("*.json"):
+            self.register_dataset_from_json_definition(f)
 
     def get_dataset(self, title: str, split: Split) -> Dataset:
         if (title, split) in self._datasets:
