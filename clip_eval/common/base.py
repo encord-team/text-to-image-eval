1bec3cb5189d07c29082aeec676d261b8e2209eb
Eloy Pérez Torres <eloy@encord.com>
2024-04-18 16:03:11 +0200

    chore: move definition to same file as embeddings

--- clip_eval/common/base.py
+++ clip_eval/common/base.py
@@ -1,12 +1,26 @@
+import logging
 from enum import StrEnum, auto
 from pathlib import Path
+from typing import Annotated, Any
 
 import numpy as np
 from pydantic import BaseModel, model_validator
+from pydantic.functional_validators import AfterValidator
 
-from clip_eval.constants import NPZ_KEYS
+from clip_eval.constants import NPZ_KEYS, PROJECT_PATHS
 
 from .numpy_types import ClassArray, EmbeddingArray
+from .string_utils import safe_str
+
+SafeName = Annotated[str, AfterValidator(safe_str)]
+logger = logging.getLogger("multiclips")
+
+
+class Split(StrEnum):
+    TRAIN = auto()
+    VALIDATION = auto()
+    TEST = auto()
+    ALL = auto()
 
 
 class Embeddings(BaseModel):
@@ -70,8 +84,54 @@ class Embeddings(BaseModel):
         arbitrary_types_allowed = True
 
 
-class Split(StrEnum):
-    TRAIN = auto()
-    VALIDATION = auto()
-    TEST = auto()
-    ALL = auto()
+class EmbeddingDefinition(BaseModel):
+    model: SafeName
+    dataset: SafeName
+
+    def _get_embedding_path(self, split: Split, suffix: str) -> Path:
+        return Path(self.dataset) / f"{self.model}_{split}{suffix}"
+
+    def embedding_path(self, split: Split) -> Path:
+        return PROJECT_PATHS.EMBEDDINGS / self._get_embedding_path(split, ".npz")
+
+    def get_reduction_path(self, reduction_name: str, split: Split):
+        return PROJECT_PATHS.REDUCTIONS / self._get_embedding_path(split, f".{reduction_name}.2d.npy")
+
+    def load_embeddings(self, split: Split) -> Embeddings | None:
+        """
+        Load embeddings for embedding configuration or return None
+        """
+        try:
+            return Embeddings.from_file(self.embedding_path(split))
+        except ValueError:
+            return None
+
+    def save_embeddings(self, embeddings: Embeddings, split: Split, overwrite: bool = False) -> bool:
+        """
+        Save embeddings associated to the embedding definition.
+        Args:
+            embeddings: The embeddings to store
+            split: The dataset split that corresponds to the embeddings
+            overwrite: If false, won't overwrite and will return False
+
+        Returns:
+            True iff file stored successfully
+
+        """
+        if self.embedding_path(split).is_file() and not overwrite:
+            logger.warning(
+                f"Not saving embeddings to file `{self.embedding_path(split)}` as overwrite is False and file exists"
+            )
+            return False
+        self.embedding_path(split).parent.mkdir(exist_ok=True, parents=True)
+        embeddings.to_file(self.embedding_path(split))
+        return True
+
+    def __str__(self):
+        return self.model + "_" + self.dataset
+
+    def __eq__(self, other: Any) -> bool:
+        return isinstance(other, EmbeddingDefinition) and self.model == other.model and self.dataset == other.dataset
+
+    def __hash__(self):
+        return hash((self.model, self.dataset))
